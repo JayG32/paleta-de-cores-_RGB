c2e70b10b5128794e82f3de9998753e576e1904c
Janiele Nogueira <njaniele@yahoo.com>
2020-07-23 20:53:24 -0300

    feita a insercao de logica para conversao de valores em rgb para hexadecimal

--- js/script.js
+++ js/script.js
@@ -1,31 +1,40 @@
-window.addEventListener('load', start);
-
-var sliderVermelho = document.querySelector('#sliderVermelho');
-var sliderVede = document.querySelector('#sliderVerde');
-var sliderAzul = document.querySelector('#sliderAzul');
-
-var inputVermelho = document.querySelector('#inputVermelho');
-var inputVerde = document.querySelector('#inputVerde');
-var inputAzul = document.querySelector('#inputAzul'); 
-
-var cxCores = document.querySelector('.cxCores');
-
-sliderVermelho.addEventListener('input', setColor);
-sliderVerde.addEventListener('input', setColor);
-sliderAzul.addEventListener('input', setColor);
+window.addEventListener('load', start)
 
 function start() {
     console.log('página carregada')
-    setColor();
+    changeRange()
+}
+
+function changeRange() {
+    var red = parseInt(document.getElementById('sliderVermelho').value)
+    var green = parseInt(document.getElementById('sliderVerde').value)
+    var blue = parseInt(document.getElementById('sliderAzul').value)
+    var cxCores = document.querySelector('.cxCores')
+    
+    var color = '#' + hex(red) + hex(green) + hex(blue)
+
+    cxCores.style.backgroundColor = color
+    document.getElementById('hex-label').innerText = color
+
+    document.getElementById('inputVermelho').value = red
+    document.getElementById('inputVerde').value = green
+    document.getElementById('inputAzul').value = blue
+
+    if (red < 100 && green < 100 && blue < 100) {
+        cxCores.style.color = 'white'
+    } else {
+        cxCores.style.color = 'black'
+    }
     
 }
 
-function setColor() {
-     inputVermelho.value = sliderVermelho.value;
-     inputVerde.value = sliderVerde.value;
-     inputAzul.value = sliderAzul.value;
+function hex(v) {
+    var hex = v.toString(16)
+    if (v < 16) {
+        hex = '0' + hex
+    }
 
-     cxCores.style.backgroundColor = `rgb(${sliderVermelho.value}, ${sliderVerde.value}, ${sliderAzul.value})`
+    return hex
 }
 
 start()
